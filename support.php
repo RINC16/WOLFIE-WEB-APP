209b404ddfc92fa8b3cfaa4988aa234aa012faab
Ebuka Peter Obiorah <154116038+RINC16@users.noreply.github.com>
2025-03-13 15:03:31 +0000

    Created support.php for rendering the student support page.
    Implemented support.php to handle the rendering of support.twig using Twig.
    
    Configured Twig environment to load templates from the designated directory.

---
+++ support.php
@@ -0,0 +1,11 @@
+<?php
+require_once 'vendor/autoload.php';
+
+// Set up Twig environment
+$loader = new \Twig\Loader\FilesystemLoader('/home/stud/1/2378831/public_html/OpenDay/templates');
+$twig = new \Twig\Environment($loader, [
+    'debug' => true
+]);
+
+// Render the Twig template
+echo $twig->render('support.twig');
