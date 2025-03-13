d532a5c205a0fa13f9887e82dc6d722f29bf7882
Ebuka Peter Obiorah <154116038+RINC16@users.noreply.github.com>
2025-03-13 13:28:13 +0000

    Create accommodation.php
    This commit adds accommodation.php, which renders the accommodation.twig template using Twig. 
    
    The file is configured to load templates from the correct directory and enable debugging.

---
+++ accommodation.php
@@ -0,0 +1,12 @@
+<?php
+require_once 'vendor/autoload.php';
+
+// Set up Twig environment
+$loader = new \Twig\Loader\FilesystemLoader('/home/stud/1/2378831/public_html/OpenDay/templates');
+$twig = new \Twig\Environment($loader, [
+    'debug' => true
+]);
+
+// Render the Twig template
+echo $twig->render('accommodation.twig'); 
+?>
